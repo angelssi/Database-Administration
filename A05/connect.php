97835c5601b04aab45a1d8d15c9791bc4c337728
Lira Angela Torralba <liratorralbasta@gmail.com>
2024-10-30 23:25:21 +0800

    activity5

--- A05/connect.php
+++ A05/connect.php
@@ -1,16 +1,16 @@
 <?php
   $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $db = "demo";
+	$dbuser = "root";
+	$dbpass = "";
+	$db = "activity4";
 
-    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
-    if(!$conn)
-    {
-        die("Connection Failed. ". mysqli_connect_error());
-        echo "can't connect to database";
-    }
+	if(!$conn)
+	{
+		die("Connection Failed. ". mysqli_connect_error());
+		echo "can't connect to database";
+	}
 
   function executeQuery($query){
     $conn = $GLOBALS['conn'];
